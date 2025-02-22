c305c1e417a6d578a4c3bc021388500b8056417f
patler044 <patler044@gmail.com>
2025-02-22 13:51:43 +0000

    Removed common comment

--- app/Front-end/components/Hero.jsx
+++ app/Front-end/components/Hero.jsx
@@ -1,8 +1,7 @@
 "use client";
 import React from "react";
 import { motion } from "framer-motion";
-import { useRouter } from "next/navigation"; // ✅ Next.js Router
-
+import { useRouter } from "next/navigation";
 const Hero = () => {
   const router = useRouter();
 
